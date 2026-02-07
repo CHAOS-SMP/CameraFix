03e75640a765fbfc76ef699ec15a8cdfa5b5f8d9
Dg32z_ <a1410521748@hotmail.com>
2026-02-07 13:40:25 +0800

    first

---
+++ src/main/java/cn/chaosmp/camerafix/Main.java
@@ -0,0 +1,11 @@
+package cn.chaosmp.camerafix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("COPYRIGHT 2026@CHAOS-SMP");
+        System.out.println("POWERED BY CHAOS-SMP (chaos-smp.cn)");
+    }
+}
